9d236aa444531047e864ccd9e5d922aac246d731
Krisna Wandhana <warriorkrisna@gmail.com>
2024-05-20 16:20:07 +0800

    Initialization Project React

---
+++ Capstone-React-6/src/pages/Home/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+import HeroSection from '../../components/HeroSection/HeroSection'
+
+export default function Homepage() {
+  return (
+    <div>
+        homepage
+        <HeroSection />
+        <Footer />
+    </div>
+
+
+  )
+}
