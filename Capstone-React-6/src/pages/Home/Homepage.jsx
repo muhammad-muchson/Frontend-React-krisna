efac4dc6aeb6f7774ecbe5155ef5f2c24f7d25da
Krisna Wandhana <warriorkrisna@gmail.com>
2024-05-25 02:09:50 +0800

    Update Homepage.jsx

--- Capstone-React-6/src/pages/Home/Homepage.jsx
+++ Capstone-React-6/src/pages/Home/Homepage.jsx
@@ -11,15 +11,6 @@ export default function Homepage() {
         <HeroSection />
         <Footer />
         <br />
-        <div className="flex flex-col">
-        <p className="font-extrabold text-heading1">Head1</p>
-        <p className="font-bold text-heading2">Head2</p>
-        <p className="font-semibold text-heading3">Head3</p>
-        <p className="font-medium text-body1">Body1</p>
-        <p className="font-regular text-body2">Body2</p>
-      </div>
     </div>
-
-
   )
 }
