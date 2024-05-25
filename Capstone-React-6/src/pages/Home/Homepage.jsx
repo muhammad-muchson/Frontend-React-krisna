24137484b93daafe635c361755ca169f16427807
Krisna Wandhana <warriorkrisna@gmail.com>
2024-05-25 13:18:57 +0800

    update sidebar

--- Capstone-React-6/src/pages/Home/Homepage.jsx
+++ Capstone-React-6/src/pages/Home/Homepage.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import SideBar from '../../components/SideBar/SideBar'
-import Footer from '../../components/Footer/Footer'
-import HeroSection from '../../components/HeroSection/HeroSection'
+// import Footer from '../../components/Footer/Footer'
+// import HeroSection from '../../components/HeroSection/HeroSection'
 
 export default function Homepage() {
   return (
     <div>
-        homepage
         <SideBar />
-        <HeroSection />
-        <Footer />
-        <br />
     </div>
   )
 }
