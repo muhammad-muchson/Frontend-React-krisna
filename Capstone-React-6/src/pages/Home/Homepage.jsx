90c9482bb7a9dcb2e20eba85a0dd26575833a460
Krisna Wandhana <warriorkrisna@gmail.com>
2024-05-31 15:38:36 +0800

    create HomePage

--- Capstone-React-6/src/pages/Home/Homepage.jsx
+++ Capstone-React-6/src/pages/Home/Homepage.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import Login from '../Login/Login'
+import React from 'react';
+import { logout } from '../../utils/auth';
+import { Navigate } from 'react-router-dom';
 
 export default function Homepage() {
+  const handleLogout = () => {
+    logout();
+    // Redirect to login page after logout
+    <Navigate to="/Login" />;
+  };
+
   return (
     <div>
-        <Login />
+      <h1>Welcome to Homepage</h1>
+      <button className='btn' onClick={handleLogout}>Logout</button>
     </div>
-  )
+  );
 }
